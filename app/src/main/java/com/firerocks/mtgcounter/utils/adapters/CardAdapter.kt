979f1f2d3b7354223ea73e78859878a79ab55187
Iseis <iseis360@gmail.com>
2018-11-06 17:22:53 -0800

    Updated search activity recycler view view holder

--- app/src/main/java/com/firerocks/mtgcounter/utils/adapters/CardAdapter.kt
+++ app/src/main/java/com/firerocks/mtgcounter/utils/adapters/CardAdapter.kt
@@ -1,5 +1,6 @@
 package com.firerocks.mtgcounter.utils.adapters
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,9 +13,13 @@ import kotlinx.android.synthetic.main.card_search_view.view.*
 
 class CardAdapter(private val cards: ArrayList<MtgCard>, private val listener: (MtgCard) -> Unit )
     : RecyclerView.Adapter<CardAdapter.CardViewHolder>() {
+
+    lateinit var mContext: Context
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
 
         val view = LayoutInflater.from(parent.context).inflate(R.layout.card_search_view, parent, false)
+        mContext = parent.context
 
         return CardViewHolder(view)
     }
@@ -27,10 +32,13 @@ class CardAdapter(private val cards: ArrayList<MtgCard>, private val listener: (
     }
 
     class CardViewHolder(val iv: View) : RecyclerView.ViewHolder(iv) {
+
         fun bind(card: MtgCard, listener: (MtgCard) -> Unit) {
-            val cardView = iv as CardView
-
-
+            val cardView = (iv as CardView).also { view ->
+                Picasso.get().load(card.imageUrl).into(view.card_image)
+                view.card_name_title.text = card.name
+                view.card_flavor_text.text = card.set
+            }
 
             cardView.setOnClickListener { listener(card) }
         }
