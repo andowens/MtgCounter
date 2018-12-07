2e9bdfa53aeb087262927b866aadd888e94a2a84
Iseis <iseis360@gmail.com>
2018-12-06 17:11:18 -0800

    Started adding bottom navigation for bet app navigation

---
+++ app/src/main/java/com/firerocks/mtgcounter/main/MtgCounterActivity.kt
@@ -0,0 +1,13 @@
+package com.firerocks.mtgcounter.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.firerocks.mtgcounter.R
+
+class MtgCounterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mtg_counter)
+    }
+}
